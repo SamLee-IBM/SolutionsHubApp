0954786dff4744900ffad34ddc2e69c2a16ac064
Samuel-L <Samuel.L@ibm.com>
2024-12-10 14:49:01 -0500

    trying this

--- api/gh_bot.js
+++ api/gh_bot.js
@@ -29,9 +29,9 @@ export function POST(request) {
 
     const octokit = github_app.getInstallationOctokit(installation_id);
 
-    console.log(request.body.json())
+    console.log(request.json())
 
-    var body = request.body.json();
+    var body = request.json();
 
     if (Object.hasOwn(body, "challenge")) {
         return new Response(body)
