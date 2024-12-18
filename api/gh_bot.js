390bb9d89fcbd091bc5b7d5212d5d6804a96f4d6
Samuel-L <Samuel.L@ibm.com>
2024-12-18 12:15:44 -0500

    log damnit

--- api/gh_bot.js
+++ api/gh_bot.js
@@ -114,7 +114,7 @@ export async function POST(request) {
             let customProps = [{"property_name": "Technology", "value": eventData['multi_select5__1']['chosenValues'].map((prop) => prop.name)},
                 { "property_name": "Industry", "value": eventData["single_select__1"]["label"]["text"]},
                 { "property_name": "Title", "value":eventData["short_text1__1"]["value"]}]
-            print(customProps)
+            console.log(customProps)
             try {
                 const propResult = await octokit.request("PATCH /repos/{org}/{repo}/properties/values", {
                     org: ce_org,
